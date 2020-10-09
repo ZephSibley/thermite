1285500bb2ac6d77c150716e519ae4f99f94782b
ZephSibley <zeph.sibley@yahoo.com>
2020-10-09 15:51:16 +0100

    Handling op constant in run

--- src/vm.rs
+++ src/vm.rs
@@ -1,5 +1,6 @@
 use crate::chunk::Chunk;
 use crate::chunk::OpCode;
+use crate::chunk::Const
 
 enum InterpretResult {
     InterpretOk,
@@ -9,20 +10,24 @@ enum InterpretResult {
 
 struct VM {
     chunk: Chunk,
+    stack: Vec<Const>
 }
 
 impl VM {
     pub fn new(chunk: Chunk) -> VM {
         VM {
             chunk,
+            stack: Vec::new(),
         }
     }
 
     pub fn run(self) -> InterpretResult {
         for (i, code) in &self.chunk {
             match code {
+                OpCode::OpConstant(ci) => {
+                    println!("{:?}\n", self.chunk.constants[*ci])
+                },
                 OpCode::OpReturn => return InterpretResult::InterpretOk,
-                OpCode::OpConstant(c) => return InterpretResult::InterpretOk,
             }
         }
         return InterpretResult::InterpretOk;
